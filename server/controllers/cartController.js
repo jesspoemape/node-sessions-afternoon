9795c751c29a17af4ea101aef2248f933652f1c9
Jess Poemape <jesspoemape@gmail.com>
2017-06-29 16:54:31 -0600

    cart controller add and delte work.

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -19,15 +19,7 @@ module.exports = {
     },
     delete: (req, res, next) => {
         if (req.query.id) {
-            for (var i = 0; i < req.session.user.cart.length; i++) {
-                if (req.query.id === req.session.user.cart[i]) {
-                    req.session.user.cart.splice(i, 1);
-                    res.status(200).send(req.session.user);
-                }
-                else {
-                    res.status(200).send(req.session.user);
-                } 
-            }
+            
         }
     },
     checkout: (req, res, next) => {
