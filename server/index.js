735ae76580dc4233406ef15aa9a6d71eb0886f8b
Jess Poemape <jesspoemape@gmail.com>
2017-06-29 13:58:57 -0600

    app has session middleware

--- server/index.js
+++ server/index.js
@@ -5,6 +5,11 @@ const session = require('express-session');
 const app = express();
 
 app.use(bodyParser.json());
+app.use(session({
+    secret: '83y98roihaejnd5r6tyg3hewds90iojk4rewfsdtyughj23eqwdyiuh',
+    resave: false,
+    saveUninitialized: true
+}));
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
