5509d249dc95f42ea8bdaf234bc2c13cf840053f
Jess Poemape <jesspoemape@gmail.com>
2017-06-29 19:13:11 -0600

    cart controller deletes items from cart

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -19,7 +19,10 @@ module.exports = {
     },
     delete: (req, res, next) => {
         if (req.query.id) {
-            
+            if (req.session.user.cart.find( product => { return product.id == req.query.id } )) { // this tells us if the product is in the cart 
+                req.session.user.cart.splice(req.session.user.cart.indexOf(req.query.id), 1);
+                res.status(200).send(req.session.user);
+            } 
         }
     },
     checkout: (req, res, next) => {
